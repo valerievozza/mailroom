f7dd376b30155def5094b7ffe31db5bb061f9cde
valerievozza <valerievozza@gmail.com>
2022-10-06 14:47:46 -0700

    change method override settings

--- app.js
+++ app.js
@@ -28,16 +28,7 @@ app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
 // Method override
-app.use(
-  methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      let method = req.body._method
-      delete req.body._method
-      return method
-    }
-  })
-)
+app.use(methodOverride("_method"));
 
 //DETERMINE LEVEL OF LOGGING
 if(process.env.NODE_ENV === 'development') {
