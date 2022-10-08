a65486e57ff262853114706a94c65201d4b88f81
valerievozza <valerievozza@gmail.com>
2022-10-08 16:40:08 -0700

    revert to passport local strategy

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,38 +10,38 @@ exports.getLogin = (req, res) => {
   res.render("login", { msg: req.flash('errors') });
 };
 
-// exports.postLogin = (req, res, next) => {
-//   const validationErrors = [];
-//   if (!validator.isEmail(req.body.email))
-//     validationErrors.push({ msg: "Please enter a valid email address." });
-//   if (validator.isEmpty(req.body.password))
-//     validationErrors.push({ msg: "Password cannot be blank." });
+exports.postLogin = (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (validator.isEmpty(req.body.password))
+    validationErrors.push({ msg: "Password cannot be blank." });
 
-//   if (validationErrors.length) {
-//     req.flash("errors", validationErrors);
-//     return res.redirect("/login");
-//   }
-//   req.body.email = validator.normalizeEmail(req.body.email, {
-//     gmail_remove_dots: false,
-//   });
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.redirect("/login");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
 
-//   passport.authenticate("local", (err, user, info) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       req.flash("errors", info);
-//       return res.redirect("/login");
-//     }
-//     req.logIn(user, (err) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       req.flash("success", { msg: "Success! You are logged in." });
-//       res.redirect(req.session.returnTo || "/clients");
-//     });
-//   })(req, res, next);
-// };
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash("errors", info);
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", { msg: "Success! You are logged in." });
+      res.redirect(req.session.returnTo || "/clients");
+    });
+  })(req, res, next);
+};
 
 exports.logout = (req, res) => {
   req.logout((err) => {
@@ -70,73 +70,73 @@ exports.getSignup = (req, res) => {
   res.render("signup", { msg: req.flash('errors') })
 };
 
-// exports.postSignup = async (req, res, next) => {
-//   const validationErrors = [];
-//   if (!validator.isEmail(req.body.email))
-//     validationErrors.push({ msg: "Please enter a valid email address." });
-//   if (!validator.isLength(req.body.password, { min: 8 }))
-//     validationErrors.push({
-//       msg: "Password must be at least 8 characters long",
-//     });
-//   if (req.body.password !== req.body.confirmPassword)
-//     validationErrors.push({ msg: "Passwords do not match" });
+exports.postSignup = async (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({
+      msg: "Password must be at least 8 characters long",
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Passwords do not match" });
 
-//   if (validationErrors.length) {
-//     req.flash("errors", validationErrors);
-//     console.log(validationErrors)
-//     return res.redirect("../signup");
-//   }
-//   req.body.email = validator.normalizeEmail(req.body.email, {
-//     gmail_remove_dots: false,
-//   });
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    console.log(validationErrors)
+    return res.redirect("../signup");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
 
-//   const user = await new User({
-//     userName: req.body.userName,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
+  const user = await new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-//   console.log(user)
+  console.log(user)
 
 
 
-//   User.findOne(
-//     { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-//     (err, existingUser) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       if (existingUser) {
-//         req.flash("errors", {
-//           msg: "Account with that email address or username already exists.",
-//         });
-//         return res.redirect("../signup");
-//       }
-//       user.save((err) => {
-//         if (err) {
-//           return next(err);
-//         }
-//         req.logIn(user, (err) => {
-//           if (err) {
-//             return next(err);
-//           }
-//           res.redirect("/clients");
-//         });
-//       });
-//     }
-//   );
-// };
+  User.findOne(
+    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    (err, existingUser) => {
+      if (err) {
+        return next(err);
+      }
+      if (existingUser) {
+        req.flash("errors", {
+          msg: "Account with that email address or username already exists.",
+        });
+        return res.redirect("../signup");
+      }
+      user.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/clients");
+        });
+      });
+    }
+  );
+};
 
-  // Get Google login
-  exports.getGoogleLogin = passport.authenticate('google', { scope: ['profile', 'email'] })
+//   // Get Google login
+//   exports.getGoogleLogin = passport.authenticate('google', { scope: ['profile', 'email'] })
 
-  // Google auth callback
-  exports.googleCallback = passport.authenticate("google", {
-    failureRedirect: "/login",
-    successRedirect: "/clients",
-    failureFlash: true,
-    successFlash: "Successfully logged in!",
-  })
+//   // Google auth callback
+//   exports.googleCallback = passport.authenticate("google", {
+//     failureRedirect: "/login",
+//     successRedirect: "/clients",
+//     failureFlash: true,
+//     successFlash: "Successfully logged in!",
+//   })
 
   exports.getDashboard = async (req, res) => {
     try{
--- models/User.js
+++ models/User.js
@@ -1,31 +1,35 @@
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true
   },
-  // password: {
-  //   type: String,
-  // },
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
   org: {
     type: String
   },
-  googleId: {
-    type: String
-  },
-  displayName: {
-    type: String
-  },
-  firstName: {
-    type: String
-  },
-  lastName: {
-    type: String
-  },
-  image: {
-    type: String
-  },
+  // googleId: {
+  //   type: String
+  // },
+  // displayName: {
+  //   type: String
+  // },
+  // firstName: {
+  //   type: String
+  // },
+  // lastName: {
+  //   type: String
+  // },
+  // image: {
+  //   type: String
+  // },
   createdAt: {
     type: Date,
     default: Date.now
@@ -35,27 +39,27 @@ const UserSchema = new mongoose.Schema({
 
 // Password hash middleware.
  
-//  UserSchema.pre('save', function save(next) {
-//   const user = this
-//   if (!user.isModified('password')) { return next() }
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) { return next(err) }
-//     bcrypt.hash(user.password, salt, (err, hash) => {
-//       if (err) { return next(err) }
-//       user.password = hash
-//       next()
-//     })
-//   })
-// })
+ UserSchema.pre('save', function save(next) {
+  const user = this
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
 
 
 // Helper method for validating user's password.
 
-// UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     cb(err, isMatch)
-//   })
-// }
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch)
+  })
+}
 
 
 module.exports = mongoose.model('User', UserSchema)
