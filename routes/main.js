761c51e553cb9c6d52649359d4c3f6fc44898614
valerievozza <valerievozza@gmail.com>
2022-10-08 11:54:28 -0700

    add google auth routes

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,11 @@ router.get('/login', ensureGuest, authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
+// Get Google Login page
+router.get('/google', authController.getGoogleLogin)
+
+// Google auth callback
+router.get('/google/callback', authController.googleCallback)
 
 router.get('/dashboard', ensureAuth, authController.getDashboard)
 
