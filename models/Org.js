a319a1f6b45b24ac94e006ce8702493ebc9fc560
valerievozza <valerievozza@gmail.com>
2022-10-13 15:47:48 -0700

    remove unused middleware

--- models/Org.js
+++ models/Org.js
@@ -18,28 +18,4 @@ const OrgSchema = new mongoose.Schema({
   }]
 })
 
-// Password hash middleware.
- 
-UserSchema.pre('save', function save(next) {
-  const user = this
-  if (!user.isModified('password')) { return next() }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err) }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { return next(err) }
-      user.password = hash
-      next()
-    })
-  })
-})
-
-
-// Helper method for validating user's password.
-
-UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch)
-  })
-}
-
 module.exports = mongoose.model('Org', OrgSchema)
