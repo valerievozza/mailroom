c2d3de0e5b16e7f43365da55d400d18684162e47
valerievozza <valerievozza@gmail.com>
2022-10-19 20:20:43 -0700

    create Reminder schema

---
+++ models/Reminder.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const ReminderSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    default: 'Your mailbox is inactive',
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  org: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Org'
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  }
+})
+
+module.exports = mongoose.model('Reminder', ReminderSchema)
